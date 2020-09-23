39911d157b6e4e0873f038d05b04b03096eba28a
giicha2 <giicha2@giicha2ui-MacBook-Pro.local>
2020-09-23 16:21:52 +0900

    add map

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import './App.css';
 import Customer from './components/Customer';
 import { render } from '@testing-library/react';
 
-const cus=[{
+const customers=[{
   'id':1,
-  'image':'https://placeimg.com/64/64/any',
+  'image':'https://placeimg.com/64/64/1',
   'name':'gichan',
   'birthday':'7501191',
   'gender':'mele',
   'job':'student'
-}
-]
-function App() {
-  return (
-    <div className="App">
-      <Customer
-        id={cus[0].id}
-        image={cus[0].image}
-        name={cus[0].name}
-        birthday={cus[0].birthday}
-        gender={cus[0].gender}
-        job={cus[0].job}
-      />
-    </div>
-  )
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'sangeun',
+  'birthday':'770412',
+  'gender':'femele',
+  'job':'wife'
+}]
+class App extends React.Component {
+  render(){
+    return(
+      <div>{
+        customers.map(c => { 
+          return(
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}/>
+          );
+          }
+        )            
+        }        
+      </div>
+    );
+  }
 }
 //test
 export default App;
